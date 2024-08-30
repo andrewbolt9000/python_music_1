31256956ffe3b7cff62d9da97835d70d7e8b413d
Andre <andre@Andres-MacBook-Pro.local>
2024-08-30 16:08:30 -0700

    guitar

--- main/delay_calc.py
+++ main/delay_calc.py
@@ -136,19 +136,7 @@ class Note:
 		# exit()
 
 
-STANDARD = 'standard'
-class Guitar:
-	_tuning = None 
-	_max_fret = 24
 
-	def __init__(self, tuning=STANDARD):
-		self._tuning = tuning 
-
-	def print_fretboard(self, note):
-		print(note)
-		
-		
-		
 
 class DelayCalculator(object):
 	"""docstring for DelayCalculator"""
